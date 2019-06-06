afefb10b927f7097898ccb7c9cfc0bb343d13fa1
JP Lew <jp@hackcapital.com>
2019-06-06 16:50:01 -0700

    add generic type to prompt function

--- src/ux.ts
+++ src/ux.ts
@@ -1,55 +1,54 @@
-import cli from 'cli-ux';
-import inquirer, { Question, Answers } from '@cto.ai/inquirer';
-import cliProgress from 'cli-progress';
-import notifier from 'node-notifier';
-import link from 'terminal-link';
-import colors from './colors';
+import cli from 'cli-ux'
+import inquirer, { Question } from '@cto.ai/inquirer'
+import cliProgress from 'cli-progress'
+import notifier from 'node-notifier'
+import link from 'terminal-link'
+import colors from './colors'
 
-const { table, tree, action } = cli;
+const { table, tree, action } = cli
 
 function url(text: string, url: string): string {
   // sdk.track(['UX', 'url'], { text, url });
-  return link(colors.multiBlue(text), url);
+  return link(colors.multiBlue(text), url)
 }
 function annotation(text: string, annotation: string): void {
   // sdk.track(['UX', 'annotation'], { text, annotation });
-  cli.annotation(text, annotation);
+  cli.annotation(text, annotation)
 }
 function notify(options: object): void {
   // sdk.track(['UX', 'notify'], options);
-  notifier.notify(options);
+  notifier.notify(options)
 }
 
-async function prompt(questions: Question[] | Question): Promise<Answers> {
+async function prompt<A>(questions: Question[] | Question): Promise<A> {
   // sdk.track(['UX', 'prompt'], questions);
-  const answers = await inquirer.prompt(questions);
-  return answers;
+  return inquirer.prompt<A>(questions)
 }
 
 function start(text: string): void {
   // sdk.track(['UX', 'spiner-start']);
-  action.start(text);
+  action.start(text)
 }
 
 function stop(text: string): void {
   // sdk.track(['UX', 'spiner-stop']);
-  action.stop(text);
+  action.stop(text)
 }
 
 async function wait(duration: number): Promise<void> {
   // sdk.track(['UX', 'wait'], { duration });
-  await cli.wait(duration);
+  await cli.wait(duration)
 }
 
 const present: cliProgress.Options = {
   format: colors.callOutCyan(' {bar} {percentage}% '),
   barCompleteChar: '\u2588',
-  barIncompleteChar: '\u2591'
-};
+  barIncompleteChar: '\u2591',
+}
 
 function init(options: cliProgress.Options = present) {
   // sdk.track(['UX', 'progress bar'], { options });
-  return new cliProgress.Bar(options);
+  return new cliProgress.Bar(options)
 }
 
 export default {
@@ -57,7 +56,7 @@ export default {
   colors,
   spinner: {
     start,
-    stop
+    stop,
   },
   wait,
   url,
@@ -67,4 +66,4 @@ export default {
   progress: { init },
   notify,
   inquirer,
-};
+}
