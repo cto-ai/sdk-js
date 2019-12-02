dfe32a6b72a11ea3f7d8dfb841f252059bc107e6
Danielle Brook-Roberge <danielle+remax@hackcapital.com>
2019-12-02 20:38:44 +0000

    Add an SDK function to expose the interface type

--- src/types.ts
+++ src/types.ts
@@ -1,24 +1,6 @@
-export interface Team {
-  id: string
-  name: string
-  createdAt: string
-  updatedAt: string
-  createdBy: string
-}
-
-export interface User {
-  id: string
-  username: string
-  emails: [
-    {
-      verified: boolean
-      address: string
-    },
-  ]
-  firstName: string
-  lastName: string
-  createdAt: string
-  updatedAt: string
+export enum Interfaces {
+  Terminal = 'terminal',
+  Slack = 'slack',
 }
 
 export interface APIError {}
